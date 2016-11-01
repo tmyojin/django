848a98004f62dd1f6d0558a09758a24ec60147b0
aaa <aaa@example.com>
2016-11-01 17:16:21 +0000

    modified:   mysite/wsgi.py 	modified:   .elasticbeanstalk/config.yml

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -8,6 +8,9 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+sys.path.insert(0, '/opt/python/current/app')
 
 from django.core.wsgi import get_wsgi_application
 
